ac32b671d238638b8cc48d07d8cda27ae045788c
Ian Lutticken <i.lutticken@ufl.edu>
2025-06-05 11:11:54 -0400

    Added the rotate icons

--- overlay.py
+++ overlay.py
@@ -27,15 +27,24 @@ class Overlay:
         }
 
         # load keyboard graphics
+        rotate_graphic = pygame.image.load(
+            os.path.join(overlay_path, "rotate-32.png")
+        ).convert_alpha()
+        self.rotate_surf = pygame.transform.scale(rotate_graphic, (20, 20))
+
         keyboard_extras = pygame.image.load(
             os.path.join(overlay_path, "Keyboard Extras.png")
         ).convert_alpha()
-        # extract space bar from coordinates (40, 80) to (60, 120)
         spacebar_original = keyboard_extras.subsurface(
             pygame.Rect(64, 32, 32, 16)  # (x, y, width, height) in pixels
         )
         # scale the spacebar to be twice as large
         self.spacebar_surf = pygame.transform.scale(spacebar_original, (64, 32))
+
+        ctrl_key_original = keyboard_extras.subsurface(
+            pygame.Rect(0, 32, 32, 16)  # (x, y, width, height) in pixels
+        )
+        self.ctrl_key_surf = pygame.transform.scale(ctrl_key_original, (64, 32))
 
     def display(self):
 
@@ -49,6 +58,25 @@ class Overlay:
             midleft=(tool_rect.right // 3, tool_rect.centery - 50)
         )
         self.display_surface.blit(self.spacebar_surf, spacebar_rect)
+
+        # ctrl key to the right of the seed icon
+        ctrl_key_rect = self.ctrl_key_surf.get_rect(
+            midleft=(
+                OVERLAY_POSITIONS["seed"][0] + 20,
+                OVERLAY_POSITIONS["seed"][1] - 20,
+            )
+        )
+        self.display_surface.blit(
+            self.ctrl_key_surf, ctrl_key_rect
+        )  # place a rotate icon above the ctrl key and another above the space bar
+        rotate_rect = self.rotate_surf.get_rect(
+            midbottom=(ctrl_key_rect.centerx - 12, ctrl_key_rect.top - 5)
+        )
+        self.display_surface.blit(self.rotate_surf, rotate_rect)
+        rotate_rect_spacebar = self.rotate_surf.get_rect(
+            midbottom=(spacebar_rect.centerx - 12, spacebar_rect.top - 5)
+        )
+        self.display_surface.blit(self.rotate_surf, rotate_rect_spacebar)
 
         # seeds
         seed_surf = self.seeds_surf[self.player.selected_seed]
